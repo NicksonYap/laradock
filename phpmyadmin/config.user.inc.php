c4e4f59ec630eaac77156cfe5e078fd17a57483e
Nickson Yap <hi@nickson.me>
2019-04-30 06:57:01 +0800

    PhpMyAdmin - Configurable and allow max_input_vars

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+//ref: https://github.com/phpmyadmin/docker#adding-custom-configuration
+
+//ref: https://docs.phpmyadmin.net/en/latest/config.html#config
+
+// $cfg['ExecTimeLimit'] = 0; //Remove execution time limit
